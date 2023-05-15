8e453b821a5512677623d4844ccf4e78e01fcde7
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-15 22:25:45 +0100

    creating a fetch in app

--- basic-fetch-data/src/App.jsx
+++ basic-fetch-data/src/App.jsx
@@ -1,26 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({})
 
+  function fetchData(){
+    fetch("https://randomuser.me/api/?results=1")
+      .then(response => response.json())
+      .then(data => setData(data)) //this is trigger re-render!!!
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => { //this one will run while building the component, ONCE!@!!!
+    fetchData()
+  },[])
+
+  console.log("Data:",data);
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+     
+      <h1>Fetch data with React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+       
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
